ea4b53bbfea6da12767cf66f64aa93fd477621f9
TheAngryVic <44233499+TheAngryVic@users.noreply.github.com>
2022-02-24 14:20:28 -0300

    Subiendo

--- DB/config.js
+++ DB/config.js
@@ -12,24 +12,3 @@ module.exports={
 db
 }
 
-// const mongoose = require('mongoose');
-
-// const dbConnection = async() =>{
-
-//     try {
-        
-//         await mongoose.connect( process.env.MONGODB_CNN, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         });
-
-//         console.log('Base de datos conectada');
-
-//     } catch (error) {
-//         console.log(error)
-//         throw new Error('Error al conectar con la base de datos ')    }
-// }
-
-// module.exports={
-//     dbConnection
-// }
